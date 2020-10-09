d106d013f7398dc377684fd7868bc8e4de94df0c
Emanuel Palm <ep__palm@hotmail.com>
2020-10-09 21:17:23 +0200

    removed old comment

--- BinaryOrchestrator/run.ps1
+++ BinaryOrchestrator/run.ps1
@@ -2,7 +2,6 @@ param($Context)
 
 Get-Member -InputObject $Context
 
-# We can only pass one object as input, but we can pass a number range as an array
 $RandomNumber = Invoke-ActivityFunction -FunctionName 'Random'
 $BitFlippedNumber = Invoke-ActivityFunction -FunctionName 'FlipBits' -Input $RandomNumber
 
